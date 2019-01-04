a3c1f7399d96e8e91f87f001e80e161ca8945192
Jijie Chen <jijie.chen@someplace.com>
2019-01-04 21:18:49 +0800

    将 vash.js 复制到 src 路径

--- copy-static-assets.js
+++ copy-static-assets.js
@@ -3,3 +3,4 @@ var shell = require('shelljs');
 shell.cp('-R', 'src/web/public/', 'dist/web/');
 shell.cp('-R', 'src/web/views/', 'dist/web/');
 shell.cp('node_modules/vash/build/vash.js', 'dist/web/public/chat-index/vash.js');
+shell.cp('node_modules/vash/build/vash.js', 'src/web/public/chat-index/vash.js');
